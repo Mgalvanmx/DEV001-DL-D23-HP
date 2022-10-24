ec42c9539a5aff909892726de9c08edc17a95e2b
LuisaRojasH <luisarojas182@hotmail.com>
2022-10-24 14:23:44 -0500

    muestra los nombres de personajes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/harrypotter/harrypotter.js';
+
+const personaje = data.characters
+personaje.forEach((characters) => {
+
+    //let nombres = document.getElementById("div1").innerHTML = characters.name;
+    let nombres2 = characters.name;
+    const div = document.createElement("container");
+    const content = document.createTextNode(nombres2);
+    div.appendChild(content);
+    let añadirDiv = document.getElementById("div1");
+    document.body.insertBefore(div, añadirDiv);
+
+    console.log(nombres2);
+});
+
+
 
 console.log(example, data);
