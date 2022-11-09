bba76e504eb5ef4c8020cf80dedd2d2bc975da51
LuisaRojasH <luisarojas182@hotmail.com>
2022-11-09 13:13:36 -0500

    muestra los personajes e info en las flip cards

--- src/main.js
+++ src/main.js
@@ -1,19 +1,106 @@
-//import { example } from './data.js';
+//import {filtrado} from './data.js';
 import data from './data/harrypotter/data.js';
 
 const personaje = data.characters;
 
 personaje.forEach((personaje) => {
     let nombre = personaje.name;
-    const article = document.createElement("article");
-    article.className = "nombres";
+    const front = document.createElement("article");
+    front.className = "front";
     const content = document.createTextNode(nombre);
-    const image = document.createElement('img');
-    image.src  = '/rayo.png';
-    image.className ="imagen"
-    article.append(content, image);
-    let addArticle = document.getElementById("section");
-    addArticle.insertAdjacentElement("beforeend", article);    
+    front.append(content);
+
+    let genero = personaje.gender
+    let especie = personaje.species;
+    if((especie === "Human") && (genero === "Male")){
+        const image2 = document.createElement("img");
+        image2.src = "img/harry.png";
+        image2.className = "imagen2";
+        front.append(image2);
+    }
+    else if((especie === "Human") && (genero === "Female")){
+        const image4 = document.createElement("img");
+        image4.src = "img/hermione.png";
+        image4.className = "imagen4";
+        front.append(image4);
+    }
+    else if (especie === "Human (formerly)") {
+        const image3 = document.createElement("img");
+        image3.src = "img/nick.png";
+        image3.className = "imagen3";
+        front.append(image3); 
+    } 
+    else{
+        const image = document.createElement('img');
+        image.src  = 'img/fowks.png';
+        image.className ="imagen";
+        front.append(image);
+    }
+
+    const {birth, death, species, ancestry, gender, patronus, house, books_featured_in} = personaje
+    const back = document.createElement("article");
+    back.className = "back";
+
+    if(birth !== null){
+        const parrafo = document.createElement("p"); 
+        parrafo.textContent = "Birth: " + birth 
+        parrafo.className = "info"
+        back.append(parrafo)
+    }
+    if(death !== null){
+        const parrafo = document.createElement("p"); 
+        parrafo.textContent = "Death: " + death 
+        parrafo.className = "info"
+        back.append(parrafo)
+    }
+    if(species !== null){
+        const parrafo = document.createElement("p"); 
+        parrafo.textContent = "Species: " + species 
+        parrafo.className = "info"
+        back.append(parrafo)
+    }
+    if(ancestry !== null){
+        const parrafo = document.createElement("p"); 
+        parrafo.textContent = "Ancestry: " + ancestry
+        parrafo.className = "info"
+        back.append(parrafo)
+    }
+    if(gender !== null){
+        const parrafo = document.createElement("p"); 
+        parrafo.textContent = "Gender: " + gender
+        parrafo.className = "info"
+        back.append(parrafo)        
+    }
+    if(patronus !== null){
+        const parrafo = document.createElement("p"); 
+        parrafo.textContent = "Patronus: " + patronus
+        parrafo.className = "info"
+        back.append(parrafo)
+    }   
+    if(house !== null){
+        const parrafo = document.createElement("p"); 
+        parrafo.textContent = "House: " + house
+        parrafo.className = "info"
+        back.append(parrafo)
+    }
+    if(books_featured_in !== null){
+        const parrafo = document.createElement("p"); 
+        parrafo.textContent = "Books Featured In: " + books_featured_in
+        parrafo.className = "info"
+        back.append(parrafo)
+    }
+
+    const addArticle = document.getElementById("section");
+    const card = document.createElement("div")
+    card.className = "card"
+    card.append(front, back)
+    addArticle.insertAdjacentElement("beforeend", card); 
 });
 
-//console.log(example, data);
+const select = document.getElementById('filtrar');
+select.addEventListener('change', 
+function filtrado() {
+    let selectedOption = this.options[select.selectedIndex];
+    let filtrar = personaje.filter(personaje => personaje.gender === selectedOption.value)
+    console.log(filtrar)
+});
